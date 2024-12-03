63396f62e5b95084aeff4583887cbc8070bfb3ac
KBrinza <KellyBrinza@gmail.com>
2024-12-03 14:37:23 -0500

    Added JQuery elements to Academy Cinemas

--- Academy-Cinemas/JAVASCRIPT/javascript.js
+++ Academy-Cinemas/JAVASCRIPT/javascript.js
@@ -1,9 +1,9 @@
 //initializes popovers//
 var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
 
-popoverTriggerList.forEach(function(element){
+popoverTriggerList.forEach(function (element) {
     var imgSrc = element.getAttribute('data-bs-img');
-    var content = "<img class='star-rating' src='" + imgSrc +"'>";
+    var content = "<img class='star-rating' src='" + imgSrc + "'>";
     new bootstrap.Popover(element, {
         content: content,
         html: true,
@@ -14,24 +14,47 @@ popoverTriggerList.forEach(function(element){
 //initializes toast//
 var toastElList = [].slice.call(document.querySelectorAll('.toast'))
 var toastList = toastElList.map(function (toastEl) {
-  return new bootstrap.Toast(toastEl)
+    return new bootstrap.Toast(toastEl)
 })
 
 //function to display toast with selected options//
-function displaySelectedMovieOptions(){
+function displaySelectedMovieOptions() {
     var movie = document.getElementById('movieSelect').options[document.getElementById('movieSelect').selectedIndex].text;
     var time = document.getElementById('timeSelect').options[document.getElementById('timeSelect').selectedIndex].text;
     var quantity = document.getElementById('quantity').value;
 
-    var message = "Purchase confirmed for: " + movie + "\nTime: " + time + "\nTickets: " +  quantity;
+    var message = "Purchase confirmed for: " + movie + "\nTime: " + time + "\nTickets: " + quantity;
 
-//display toast//
+    //display toast//
     var toastBody = document.getElementById('toastBody');
     toastBody.textContent = message;
     var toast = new bootstrap.Toast(document.getElementById('toastDisplay'));
     toast.show()
 }
 
-function buyTickets(){
+function buyTickets() {
     displaySelectedMovieOptions();
 }
+
+//JQUERY//
+//shrinks header when document is scrolled down by 80 pixels//
+$(document).on("scroll", function () {
+    //when webpage is scrolled down from top by 50px this if will trigger//
+    if ($(document).scrollTop() > 50) {
+        //once 50px requirement is met, ass nav-shrink class selector to //
+        //html elements with nav class//
+        $("nav").addClass("nav-shrink");
+        //adjusts position of mobile drop menu to accomodate new height decrease//
+        $("div.navbar-collapse").css("margin-top", "-6px");
+    } else {
+        //removes nav-shrink class selector when user is at top of webpage//
+        $("nav").removeClass("nav-shrink");
+        $("div.navbar-collapse").css("margin-top", "14px");
+    }
+
+    //Close mobile menu when navigation link is clicked//
+    $(document).ready(function () {
+        $(".navbar-collapse").collapse('hide');
+    });
+});
+
